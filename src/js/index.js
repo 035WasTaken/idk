59aedbefa41cf9c0967e849a6a4af34105b2dcf9
035WasTaken <60830475+035WasTaken@users.noreply.github.com>
2021-05-11 23:06:52 -0400

    needs a few fixes (button merges with input element when screen is smaller, "style" is undefined in index.js) but pushing so i can go to bed

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,22 @@ if(!window.indexedDB) {
 }
 
 function enable(element) {
-    
+
 }
   
-$(document).ready(function() {
-    $( "#inputField" ).focus();
-    
-})
+document.onload = function() {
+    const inputField = $( "#inputField" );
+    inputField.focus();
+
+    inputField.on("change", function() {
+        const submitButton = $( "submitButton" );
+        if(inputField.value && inputField.value.length > 0) {
+            submitButton.disabled = "true";
+            return submitButton.style.opacity = 0.3;
+        }
+
+        console.log(submitButton.style)
+        submitButton.style.opacity = 1;
+        submitButton.disabled = "false";
+    })
+}
