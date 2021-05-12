3121dc9a9b39b7906c86ca8f4ca5e313107fe537
035WasTaken <60830475+035WasTaken@users.noreply.github.com>
2021-05-11 22:35:33 -0400

    Set some things up. Added HTML, CSS, and JS for basic structure

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+if(!window.indexedDB) {
+    console.warn("Your browser does not support a stable version of IndexedDB, you may not have access to some features if you do not upgrade.")
+}
+  
+$(document).ready(function() {
+    $( "#inputField" ).focus();
+})
+
+    
