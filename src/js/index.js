02a9167c90302b720de43fccffc548f38a3c27be
035WasTaken <60830475+035WasTaken@users.noreply.github.com>
2021-05-12 22:56:35 -0400

    Changed the background color, added logo, and added ACTUAL URL validation :)

--- src/js/index.js
+++ src/js/index.js
@@ -24,10 +24,14 @@ function disableButton(button) {
 
 window.onload = function () {
     const inputField = document.getElementById("inputField");
-    const submitButton = document.getElementById("submitButton")
+    const submitButton = document.getElementById("submitButton");
+    // From the wonderous world of stackoverflow.com, weighing in at an impressive 214 bytes... I bring you, URL VALIDATIONNNNNNNNNNNNNNNNNNNNNNNNNNNN
+    const urlValidateRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     inputField.focus();
     
     inputField.addEventListener("input", function() {
-        inputField.value.length > 0 ? enableButton(submitButton) : disableButton(submitButton);
+        const val = inputField.value;
+
+        val.match(urlValidateRegex) ? enableButton(submitButton) : disableButton(submitButton);
     })
 }
